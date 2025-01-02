d389b9e40a2225b375b901e911362b3de3635d26
Nguyễn Ngọc Giang <ngocgiang1832004@gmail.com>
2025-01-02 10:36:13 +0700

    add switch site for admin

--- src/components/shop/shopRoute.js
+++ src/components/shop/shopRoute.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const shopController = require("./ShopController");
 const productRouter = require("./product/productRoute");
-const ensureAuthenticated = require("../user/auth/authModel");
-const cartRoute = require("../user/cart/cartRoute");
 const userRoute = require("../user/userRoute");
 
 router.get("/api", shopController.pagination);
